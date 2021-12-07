d6948cefffbf11868fe70bae107a30672ee6bc6a
Fahri Izuddin Zulkarnaen <fahrizulkarnaen1@gmail.com>
2021-12-07 10:49:53 +0700

    final

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
         $uploadDate= date("Y-m-d", $uploadDate); 
         $uploadDate= new DateTime($uploadDate);
         $tanggal2 = date_create();
-        $jarak = $uploadDate->diff($tanggal2);
+        $jarak = $uploadDate->diff($tanggal2); // buat mencari jarak
         
         $jarakAsli = function($nb,$str){global $jaraknya; return $jaraknya .=" ".$nb.$str;}; // adds plurals
         if ($jarak->y !== 0) {
@@ -18,29 +18,24 @@
         return $jaraknya;
     }
 
-    function singkat_angka($n, $presisi=2) {
+    function singkat_angka($n) {
         if ($n < 900) {
-            $format_angka = number_format($n, $presisi);
+            $format_angka = number_format($n);
             $simbol = '';
         } else if ($n < 900000) {
-            $format_angka = number_format($n / 1000, $presisi);
+            $format_angka = number_format($n / 1000);
             $simbol = 'rb';
         } else if ($n < 900000000) {
-            $format_angka = number_format($n / 1000000, $presisi);
+            $format_angka = number_format($n / 1000000);
             $simbol = 'jt';
         } else if ($n < 900000000000) {
-            $format_angka = number_format($n / 1000000000, $presisi);
+            $format_angka = number_format($n / 1000000000);
             $simbol = 'M';
         } else {
-            $format_angka = number_format($n / 1000000000000, $presisi);
+            $format_angka = number_format($n / 1000000000000);
             $simbol = 'T';
         }
-    
-        if ( $presisi > 0 ) {
-            $pisah = '.' . str_repeat( '0', $presisi );
-            $format_angka = str_replace( $pisah, '', $format_angka );
-        }
-        
+
         return $format_angka . $simbol;
     }
 ?>
